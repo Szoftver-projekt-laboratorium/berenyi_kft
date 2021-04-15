e66d695d782365eb1338621c50383436a18f0d3f
Balint Radnai <radnai.balint@edu.bme.hu>
2021-04-15 04:53:28 +0200

    Add Proto.load() and Proto.main()
    A Proto.load() a segédfüggvényeivel majdnem kész.

--- src/berenyi_kft/Proto.java
+++ src/berenyi_kft/Proto.java
@@ -1,8 +1,11 @@
 package berenyi_kft;
 
-import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
+
+import berenyi_kft_test.Tester;
 
 public class Proto {
 	
@@ -19,15 +22,17 @@ public class Proto {
 	
 	
 	public static String getId(Object o) {
-        if(ids.containsKey(o)) {
+        if (ids.containsKey(o)) {
             return ids.get(o);
         }
         return "null";
     }
 
 	public static Object getObject(String id) {
-		if (id.equals("null"))
-			return null;
+		// TODO: Megengedjunk null id-ju objektumot? Inkabb ne.
+		// De amugy sem olvasnank be oket.
+		// if (id.equals("null"))
+		//	  return null;
 		
 		for (Map.Entry<Object, String> e : ids.entrySet()) {
 			if (e.getValue().equals(id))
@@ -52,29 +57,130 @@ public class Proto {
 		tabs--;
 	}
 	
-	public static void load(File fIn) {
-		/* 1. beolvassa a fejlecet, ures sorig
+	/**
+	 * Segedfuggveny a beolvasott objektumok peldanyositasahoz.
+	 * @param typename A letrehozando objektum tipusa szoveges azonositoval
+	 * @return A letrehozott uj objektum
+	 */
+	private static Object createObject(String typename) {
+		switch (typename) {
+			case "Controller": return new Controller();
+			case "Player": return new Player();
+			case "Game": return new Game();
+			case "Recipe": return new Recipe();
+			case "Timer": return new Timer(0, 0);
+			case "Sun": return new Sun();
+			case "Asteroid": return new Asteroid();
+			case "Coal": return new Coal();
+			case "Iron": return new Iron();
+			case "Ice": return new Ice();
+			case "Uranium": return new Uranium();
+			case "Settler": return new Settler();
+			case "AIRobot": return new AIRobot(null);
+			case "UFO": return new UFO();
+			case "TeleportingGate": return new TeleportingGate();
+			
+			// TODO: Ha invalid a tipus, akkor itt dobjunk kivetelt?
+			// Egyelore null-lal ter vissza.
+			default:
+			//	throw new NullPointerException("Invalid class name: " + typename);
+				return null;
+		}
+	}
+	
+	/**
+	 * Segedfuggveny az objektumok attributumai beallitasahoz.
+	 * @param o Az objektum, amelynek attributumait be kell tolteni
+	 * @param typename Az objektum tipusa szoveges azonositoval
+	 * @param sc Az objektum attributumai beolvasasat vegzo Scanner
+	 */
+	private static void loadObjectAttributes(Object o, String typename, Scanner sc) {
+		switch (typename) {
+			case "Controller": Controller controller = (Controller)o; controller.load(sc); break;
+			case "Player": Player p = (Player)o; /*p.load(sc);*/ break;
+			case "Game": Game game = (Game)o; /*game.load(sc);*/ break;
+			case "Recipe": Recipe r = (Recipe)o; /*r.load(sc);*/ break;
+			case "Timer": Timer timer = (Timer)o; /*timer.load(sc);*/ break;
+			case "Sun": Sun sun = (Sun)o; sun.load(sc); break;
+			case "Asteroid": Asteroid a = (Asteroid)o; a.load(sc); break;
+			case "Coal": Coal co = (Coal)o; /*co.load(sc);*/ break;
+			case "Iron": Iron ir = (Iron)o; /*ir.load(sc);*/ break;
+			case "Ice": Ice ic = (Ice)o; /*ic.load(sc);*/ break;
+			case "Uranium": Uranium ur = (Uranium)o; /*ur.load(sc);*/ break;
+			case "Settler": Settler s = (Settler)o; /*s.load(sc);*/ break;
+			case "AIRobot": AIRobot air = (AIRobot)o; /*air.load(sc);*/ break;
+			case "UFO": UFO ufo = (UFO)o; /*ufo.load(sc);*/ break;
+			case "TeleportingGate": TeleportingGate tg = (TeleportingGate)o; /*tg.load(sc);*/ break;
+			default: break;
+		}
+	}
+	
+	/**
+	 * Betolti a megadott nevu konfiguracios fajl tartalmat,
+	 * es az ids Map-ben nyilvantartja a beolvasott objektumokat.
+	 * @param filename A beolvasando jatekkonfiguracios fajl neve
+	 */
+	public static void load(String filename) throws IOException {
+		/* 0. Kitakaritja a meglevo ids Map-et.
+		 * 
+		 * 1. Beolvassa a fejlecet, ures sorig
 		 *    kozben peldanyosit mindenkit es bepakolja oket az ids-be,
-		 *    plusz egy ArrayList<Object>-be is, hogy sorban maradjanak (vagy TreeMap)
+		 *    plusz egy ArrayList<Object>-be is, hogy sorban maradjanak (vagy TreeMap).
 		 *    
 		 * 2. A lista szerint minden objektumot beolvas. A Scannert atveszi.
 		 *    (A fejlec ellenorzesre jo lesz, de nem fontos.)
 		 *    Az attributumait sorban beolvassa (mindegyiket meg kell adni?).
 		 *    Elfogyaszt egy ures sort is, majd visszater.
 		 */
+		ids.clear();
+		Map<Object, String> objectTypes = new HashMap<Object, String>();
+		
+		Scanner sc = new Scanner(filename);
+		String line = sc.nextLine();
+		while (!line.equals("")) {
+			String[] tokens = line.split("\\s");
+			String typename = tokens[0];
+			
+			for (int i = 1; i < tokens.length; i++) {
+				Object o = createObject(typename);
+				// TODO: Ha invalid a tipus, akkor itt dobjunk kivetelt?
+				// if (o == null)
+				//    throw new NullPointerException("Invalid class name: " + typename);
+				// (Lehet IllegalArgumentException is akar.)
+				if (o != null) {
+					ids.put(o, tokens[i]);
+					objectTypes.put(o, typename);
+				}
+			}
+			line = sc.nextLine();
+		}
+		
+		for (Map.Entry<Object, String> e : objectTypes.entrySet()) {
+			Object o = e.getKey();
+			String typename = e.getValue();
+			loadObjectAttributes(o, typename, sc);
+		}
+		sc.close();
 	}
 	
-	public static void save(File fOut) {
+	public static void save(String filename) {
 		
 	}
 	
 	public static void main(String[] args) {
-		// Az elejen itt dontjuk el, hogy a proto jatekot
-		// vagy a tesztet futtatjuk.
-		
-		//while(controller.state != State.EXITED) {
-			//TODO 
-		//}
+		try {
+			Scanner sc = new Scanner();
+			System.out.println("Welcome in berenyi_kft's Proto program!");
+			System.out.println("Please select whether you wish to test (Y) or play (n) the prototype game. [Y/n]: ");
+			String choice = sc.next();
+			if (choice.substring(0, 1).toLowerCase().equals("y")) {
+				Tester.testerMain(args);
+			}
+			else {
+				System.out.println("Now you can play the game.");
+				// TODO
+			}
+		}
 	}
 	
 }
